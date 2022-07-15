946ed6ef902eaa30a6be8356fe9f3bf65e43d139
jibaek <jibaek@jibaekui-MacBookPro.local>
2022-07-15 10:39:06 +0900

    working model

--- latent-diffusion/app.py
+++ latent-diffusion/app.py
@@ -1,5 +1,5 @@
-
 import sys
+
 sys.path.append("..")
 sys.path.append('../taming-transformers')
 
@@ -9,40 +9,42 @@ import os
 """Now, download the checkpoint (~5.7 GB). This will usually take 3-6 minutes."""
 
 # Commented out IPython magic to ensure Python compatibility.
-#@title Download model
+# @title Download model
 # %cd /content/latent-diffusion
 
-model_path = "./ai_models"
+model_path = "./models/ldm/text2img-large"
 outputs_path = "./content/outputs"
 
-#os.system(f"mkdir -p {model_path}")
-#os.system(f'mkdir -p {outputs_path}')
+# os.system(f"mkdir -p {model_path}")
+# os.system(f'mkdir -p {outputs_path}')
 
 import os
+
 if os.path.isfile(f"{model_path}/latent_diffusion_txt2img_f8_large.ckpt"):
-    print("Using saved model from local Drive")
-else:    
-    os.system(f'wget -O {model_path}/latent_diffusion_txt2img_f8_large.ckpt https://ommer-lab.com/files/latent-diffusion/nitro/txt2img-f8-large/model.ckpt')
+    print("Using saved model from Google Drive")
+else:
+    os.system(
+        f'wget -O {model_path}/latent_diffusion_txt2img_f8_large.ckpt https://ommer-lab.com/files/latent-diffusion/nitro/txt2img-f8-large/model.ckpt')
 
 """Let's also check what type of GPU we've got."""
 
-
 """Load it."""
 
-#@title loading utils
+# @title loading utils
 import torch
 from omegaconf import OmegaConf
 
 from ldm.util import instantiate_from_config
 
-#@title Import stuff
+# @title Import stuff
 import argparse, os, sys, glob
 import torch
 import numpy as np
 from omegaconf import OmegaConf
 from PIL import Image
 from tqdm.auto import tqdm, trange
-tqdm_auto_model = __import__("tqdm.auto", fromlist=[None]) 
+
+tqdm_auto_model = __import__("tqdm.auto", fromlist=[None])
 sys.modules['tqdm'] = tqdm_auto_model
 from einops import rearrange
 from torchvision.utils import make_grid
@@ -55,7 +57,8 @@ from open_clip import tokenizer
 import open_clip
 import tensorflow as tf
 
-#@title Load necessary functions
+
+# @title Load necessary functions
 
 
 def load_safety_model(clip_model):
@@ -100,22 +103,26 @@ def load_safety_model(clip_model):
 
     return loaded_model
 
+
 def is_unsafe(safety_model, embeddings, threshold=0.5):
     """find unsafe embeddings"""
     nsfw_values = safety_model.predict(embeddings, batch_size=embeddings.shape[0])
     x = np.array([e[0] for e in nsfw_values])
-    #print(x)
+    # print(x)
     return True if x > threshold else False
-#NSFW CLIP Filter
-#safety_model = load_safety_model("ViT-B/32")
-#clip_model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='openai')
+
+
+# NSFW CLIP Filter
+# safety_model = load_safety_model("ViT-B/32")
+clip_model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='openai')
+
 
 def load_model_from_config(config, ckpt, verbose=False):
     print(f"Loading model from {ckpt}")
-
     pl_sd = torch.load(ckpt, map_location="cuda:0")
     sd = pl_sd["state_dict"]
     model = instantiate_from_config(config.model)
+    print("after load model")
     m, u = model.load_state_dict(sd, strict=False)
     if len(m) > 0 and verbose:
         print("missing keys:")
@@ -124,18 +131,19 @@ def load_model_from_config(config, ckpt, verbose=False):
         print("unexpected keys:")
         print(u)
 
-    model = model.half()#.cuda()
+    model = model.half().cuda()
     model.eval()
     return model
 
-config = OmegaConf.load("configs/latent-diffusion/txt2img-1p4B-eval.yaml") 
+
+config = OmegaConf.load("configs/latent-diffusion/txt2img-1p4B-eval.yaml")
 model = load_model_from_config(config, f"{model_path}/latent_diffusion_txt2img_f8_large.ckpt")
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-#model = model.to(device)
+model = model.to(device)
+
+
 def run(opt):
-    print("run run")
-    print(opt)
     torch.cuda.empty_cache()
     gc.collect()
     if opt.plms:
@@ -143,18 +151,17 @@ def run(opt):
         sampler = PLMSSampler(model)
     else:
         sampler = DDIMSampler(model)
-    
+
     os.makedirs(opt.outdir, exist_ok=True)
     outpath = opt.outdir
 
     prompt = opt.prompt
 
-
     sample_path = os.path.join(outpath, "samples")
     os.makedirs(sample_path, exist_ok=True)
     base_count = len(os.listdir(sample_path))
 
-    all_samples=list()
+    all_samples = list()
     with torch.no_grad():
         with torch.cuda.amp.autocast():
             with model.ema_scope():
@@ -163,37 +170,34 @@ def run(opt):
                     uc = model.get_learned_conditioning(opt.n_samples * [""])
                 for n in trange(opt.n_iter, desc="Sampling"):
                     c = model.get_learned_conditioning(opt.n_samples * [prompt])
-                    shape = [4, opt.H//8, opt.W//8]
+                    shape = [4, opt.H // 8, opt.W // 8]
                     samples_ddim, _ = sampler.sample(S=opt.ddim_steps,
-                                                    conditioning=c,
-                                                    batch_size=opt.n_samples,
-                                                    shape=shape,
-                                                    verbose=False,
-                                                    unconditional_guidance_scale=opt.scale,
-                                                    unconditional_conditioning=uc,
-                                                    eta=opt.ddim_eta)
+                                                     conditioning=c,
+                                                     batch_size=opt.n_samples,
+                                                     shape=shape,
+                                                     verbose=False,
+                                                     unconditional_guidance_scale=opt.scale,
+                                                     unconditional_conditioning=uc,
+                                                     eta=opt.ddim_eta)
 
                     x_samples_ddim = model.decode_first_stage(samples_ddim)
-                    x_samples_ddim = torch.clamp((x_samples_ddim+1.0)/2.0, min=0.0, max=1.0)
+                    x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
 
                     for x_sample in x_samples_ddim:
                         x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
                         image_vector = Image.fromarray(x_sample.astype(np.uint8))
-                        print("before preprocess") #
                         image = preprocess(image_vector).unsqueeze(0)
-                        print("after preprocess") #
                         with torch.no_grad():
-                          image_features = clip_model.encode_image(image)
+                            image_features = clip_model.encode_image(image)
                         image_features /= image_features.norm(dim=-1, keepdim=True)
                         query = image_features.cpu().detach().numpy().astype("float32")
-                        unsafe = is_unsafe(safety_model,query,opt.nsfw_threshold)
-                        if(not unsafe):
-                            image_vector.save(os.path.join(sample_path, f"{base_count:04}.png"))
-                        else:
-                          raise Exception('Potential NSFW content was detected on your outputs. Try again with different prompts. If you feel your prompt was not supposed to give NSFW outputs, this may be due to a bias in the model')
+                        # unsafe = is_unsafe(safety_model,query,opt.nsfw_threshold)
+                        # if(not unsafe):
+                        image_vector.save(os.path.join(sample_path, f"{base_count:04}.png"))
+                        # else:
+                        #  raise Exception('Potential NSFW content was detected on your outputs. Try again with different prompts. If you feel your prompt was not supposed to give NSFW outputs, this may be due to a bias in the model')
                         base_count += 1
                     all_samples.append(x_samples_ddim)
-
 
     # additionally, save as grid
     grid = torch.stack(all_samples, 0)
@@ -202,10 +206,11 @@ def run(opt):
 
     # to image
     grid = 255. * rearrange(grid, 'c h w -> h w c').cpu().numpy()
-    
+
     Image.fromarray(grid.astype(np.uint8)).save(os.path.join(outpath, f'{prompt.replace(" ", "-")}.png'))
-    display(Image.fromarray(grid.astype(np.uint8)))
-    #print(f"Your samples are ready and waiting four you here: \n{outpath} \nEnjoy.")
+    # display(Image.fromarray(grid.astype(np.uint8)))
+    # print(f"Your samples are ready and waiting four you here: \n{outpath} \nEnjoy.")
+
 
 """# Do the run
 
@@ -215,24 +220,25 @@ Despite how impressive being able to turn text into image is, beware to the fact
 The model was trained on an unfiltered version the LAION-400M dataset, which scrapped non-curated image-text-pairs from the internet (the exception being the the removal of illegal content) and is meant to be used for research purposes, such as this one. <a href='https://laion.ai/laion-400-open-dataset/' target='_blank'>You can read more on LAION's website</a>
 """
 
-#@title Parameters
+# @title Parameters
 import argparse
-Prompt = "A mecha robot holding a sign that reads: 'Is AI art, art?" #@param{type:"string"}
-Steps = 50 #@param {type:"integer"}
-ETA = 0.0 #@param{type:"number"}
-Iterations = 2 #@param{type:"integer"}
-Width=64 #@param{type:"integer"}
-Height=64 #@param{type:"integer"}
-Samples_in_parallel=3 #@param{type:"integer"}
-Diversity_scale=5.0 #@param {type:"number"}
-PLMS_sampling=True #@param {type:"boolean"}
 
-args = argparse.Namespace( # this is opt in function "run"
-    prompt = Prompt, 
+Prompt = "Dog on horseback wearing a cowboy hat exploring the desert"  # @param{type:"string"}
+Steps = 50  # @param {type:"integer"}
+ETA = 0.0  # @param{type:"number"}
+Iterations = 4  # @param{type:"integer"}
+Width = 256  # @param{type:"integer"}
+Height = 256  # @param{type:"integer"}
+Samples_in_parallel = 2  # @param{type:"integer"}
+Diversity_scale = 5.0  # @param {type:"number"}
+PLMS_sampling = True  # @param {type:"boolean"}
+
+args = argparse.Namespace(
+    prompt=Prompt,
     outdir=f'{outputs_path}',
-    ddim_steps = Steps,
-    ddim_eta = ETA,
-    n_iter = Iterations,
+    ddim_steps=Steps,
+    ddim_eta=ETA,
+    n_iter=Iterations,
     W=Width,
     H=Height,
     n_samples=Samples_in_parallel,
@@ -241,4 +247,3 @@ args = argparse.Namespace( # this is opt in function "run"
     nsfw_threshold=0.5
 )
 run(args)
-
